f2341e62108ab6a11122770527878ac646e9499f
TechJedi51 <github.vendor@sealsystems.net>
2021-01-14 14:34:47 -0800

    First upload

---
+++ MarchCovid.widget/MarchCovid.jsx
@@ -0,0 +1,43 @@
+import { styled } from "uebersicht";
+import { css } from "uebersicht";
+
+export const refreshFrequency = 28800; //Update every 8 hours
+const Wrapper = styled("div")``;
+
+export const render = ({days}, dispatch) => {
+	var covidStartDate = new Date("03/01/2020"); 
+	let dateNow = new Date();
+	
+	// To calculate the time difference of two dates 
+	var Difference_In_Time = dateNow.getTime() - covidStartDate.getTime(); 
+  
+	// To calculate the no. of days between two dates 
+	var Difference_In_Days = Math.round(Difference_In_Time / (1000 * 3600 * 24)); 
+	
+	return (
+		<TimeWrapper>{Difference_In_Days}</TimeWrapper>
+		)
+}
+
+export const className = `
+	top: 100px;
+	left: 1200px;
+	background-image: url(MarchCovid.widget/Mar.png);
+	height: 128px;
+	margin: 0px auto;
+	text-align: center;
+	border-width: 0px;
+	border-style: none;
+	font-family: Helvetica Neue;
+	width: 128px;
+    `
+    
+const TimeWrapper = styled("div")`
+	font-size: 2vw;
+	-webkit-transform: rotate(349deg);
+	padding-top: 50px;
+	padding-right: 9px;
+	font-weight: bold;
+	color: black;
+`
+;
---
+++ README.md
@@ -0,0 +1,6 @@
+# March Covid Desktop Widget for Mac OS
+![march-covid](screenshot.png)
+## Installation
+1. Download [Übersicht](http://tracesof.net/uebersicht/)
+2. Copy `MarchCovid.widget` to your widgets folder.
+
